c7572f871a304d448ef44be4f52306aaa6d7f3c4
asilbek <asilbekmirolimov@gmail.com>
2024-03-06 19:56:49 +0500

    add template static and urls

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from shop.models import ProductCategory, Product, ProductImage, ProductReview
+
 # Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductReview)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from shop.models import Product
+
 
 # Create your views here.
+
+# FBV - function based views
+# CBV - class based views
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'templates/shop/shop.html'
